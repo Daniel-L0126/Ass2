c5681991cb1f7b9a6fa67c48693e329ee260ea23
Your Name <dxl746@case.edu>
2023-03-30 03:29:01 -0400

    ver 0.12

--- RDT/src/PacketHeader.java
+++ RDT/src/PacketHeader.java
@@ -1,8 +1,8 @@
 public class PacketHeader {
-    int type;
-    int seq_num;
-    int length; 
-    int checksum;
+    public int type;
+    public int seq_num;
+    public int length; 
+    public int checksum;
 
     public PacketHeader(int type, int seq_num, int length, int checksum){
         this.type = type;
--- RDT/src/UnreliableSocket.java
+++ RDT/src/UnreliableSocket.java
@@ -1,15 +1,53 @@
-public class UnreliableSocket{
-    int ipAddress, port;
+import java.net.*;
 
-    // This method bind the assigned ip address and port to a socket
-    public boolean bind(int ipAddress, int port){
-        this.ipAddress = ipAddress; 
-        this.port = port;
-        return true;
+public class UnreliableSocket {
+    private DatagramSocket socket;
+    private double lossProbability;
+    private double delayProbability;
+    private int delayMillis;
+    private double corruptProbability;
+
+
+    public UnreliableSocket(int port, double lossProbability) throws SocketException {
+        this.socket = new DatagramSocket(port);
+        this.lossProbability = lossProbability;
     }
 
-    public boolean recvfrom(){
-        
-        return true;
+    public void bind(int port) throws SocketException {
+        this.socket.bind(new InetSocketAddress(port));
+    }
+
+    public void sendto(byte[] data, InetAddress address, int port) throws Exception {
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+        this.socket.send(packet);
+    }
+
+    public byte[] recvfrom() throws Exception {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        this.socket.receive(packet);
+    
+        // Simulate packet loss
+        if (Math.random() < this.lossProbability) {
+            return null;
+        }
+    
+        // Simulate packet delay
+        if (this.delayProbability > 0 && Math.random() < this.delayProbability) {
+            Thread.sleep(this.delayMillis);
+        }
+    
+        // Simulate packet corruption
+        if (this.corruptProbability > 0 && Math.random() < this.corruptProbability) {
+            int corruptIndex = (int) (Math.random() * packet.getLength());
+            buffer[corruptIndex] ^= 0xFF;
+        }
+    
+        return packet.getData();
+    }
+    
+
+    public void close() {
+        this.socket.close();
     }
 }
